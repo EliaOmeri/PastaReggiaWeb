d5fc417b2e1e118f119f22ea1c1c8ebeb56310e5
elia-omeri <elia.omeri@crystal-system.eu>
2023-02-28 12:22:40 +0100

    init

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css',
+  './about1.css'
+
+]
 })
 export class AboutComponent {
 
